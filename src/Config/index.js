a6035da44511cc1e7a347b6965c399397c5a2772
Nicolas-NDK <nickquintanilha84@gmail.com>
2023-04-16 19:05:55 -0300

    RocketHelp

---
+++ src/Config/index.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBt8OG-JQCFDThHSkYlz4dBSvoomH62B3I",
+  authDomain: "rockethelp-dd28c.firebaseapp.com",
+  projectId: "rockethelp-dd28c",
+  storageBucket: "rockethelp-dd28c.appspot.com",
+  messagingSenderId: "292706728550",
+  appId: "1:292706728550:web:608e7d60740a162e440ba7"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default Firebase;
